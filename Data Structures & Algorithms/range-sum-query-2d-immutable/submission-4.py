d1894b28f898c029376bec34a9fcc0e34ca798c1
Risheek kumar B <b.risheekkumar@gmail.com>
2026-06-21 15:11:37 +0530

    Add: range-sum-query-2d-immutable - submission-4

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-4.py
@@ -0,0 +1,21 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        m, n = len(matrix), len(matrix[0])
+        self.prefix = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                self.prefix[i + 1][j + 1] = (
+                    matrix[i][j]
+                    + self.prefix[i][j + 1]
+                    + self.prefix[i + 1][j]
+                    - self.prefix[i][j]
+                )
+
+    def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
+        return (
+            self.prefix[r2 + 1][c2 + 1]
+            - self.prefix[r1][c2 + 1]
+            - self.prefix[r2 + 1][c1]
+            + self.prefix[r1][c1]
+        )
