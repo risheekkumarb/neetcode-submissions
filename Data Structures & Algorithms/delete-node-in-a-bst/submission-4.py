0b1af6353c1763b4b8c08ead7da703ab4316d1c8
Risheek kumar B <b.risheekkumar@gmail.com>
2026-07-02 16:06:34 +0530

    Add: delete-node-in-a-bst - submission-4

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root: return None
+        if key > root.val:
+            root.right = self.deleteNode(root.right,key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left,key)
+        else:
+            if not root.left: return root.right
+            elif not root.right: return root.left
+            
+            cur = root.right
+            while cur.left: cur = cur.left
+            cur.left = root.left
+            return root.right
+
+        return root
