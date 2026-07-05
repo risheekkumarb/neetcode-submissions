e033c903ea51859717018b3c840b2f0d61ac0c0a
Risheek kumar B <b.risheekkumar@gmail.com>
2026-07-05 16:59:41 +0530

    Add: permutations-ii - submission-17

---
+++ Data Structures & Algorithms/permutations-ii/submission-17.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        n = len(nums)
+        flags = [False] * n
+
+        res = []
+        path = []
+
+        def dfs():
+            if len(path) == n:
+                res.append(path[:])
+                return
+            
+            for j in range(n):
+                if flags[j]: continue
+                if j and nums[j] == nums[j-1] and not flags[j-1]: continue
+                flags[j] = True
+                path.append(nums[j])
+                dfs()
+                flags[j] = False
+                path.pop()
+
+        dfs()
+        return res
