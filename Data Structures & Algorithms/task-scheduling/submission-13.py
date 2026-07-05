f53526e01ab6b9b20af73318d7b33da1d3d87791
Risheek kumar B <b.risheekkumar@gmail.com>
2026-07-05 12:35:39 +0530

    Add: task-scheduling - submission-13

---
+++ Data Structures & Algorithms/task-scheduling/submission-13.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from heapq import heappop, heapify, heappush
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        tasks = [-o for o in Counter(tasks).values()]
+        waits = []
+        heapify(tasks); heapify(waits)
+
+        # looop
+        ## check if any waits is over and add it to tasks
+        ## take the highest freq task in the tasks
+        ## decrement its count, if count > 0 add it to waits
+        t = 0
+        while tasks or waits:
+            if not tasks: t = waits[0][0]
+            else: t += 1
+            while waits and waits[0][0] <= t:
+                time, cnt = heappop(waits)
+                heappush(tasks, (cnt))
+            cnt = 1 + heappop(tasks)
+            if cnt: heappush(waits, (t+n+1,cnt))
+        return t
