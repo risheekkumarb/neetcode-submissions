35457d5c5aac1449c66b07fb8bd7de76bea10d0b
Risheek kumar B <b.risheekkumar@gmail.com>
2026-07-12 16:03:38 +0530

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res, path = set(), []
+        cols, diag, diag1 = set(), set(), set()
+
+        def dfs(r):
+            if r == n:
+                res.add(''.join(path[:]))
+                return
+
+            for c in range(n):
+                if c in cols or r+c in diag or r-c in diag1: continue
+                QP = '.'*c + 'Q' + '.'*(n-c+1)
+                cols.add(c); diag.add(r+c); diag1.add(r-c)
+                path.append(QP)
+                dfs(r+1)
+                path.pop()
+                cols.remove(c); diag.remove(r+c); diag1.remove(r-c)
+        
+        dfs(0)
+        return len(res)
